f6900c5600e5d9059b662f09c9090b3d5fcedee1
DarkSquirr3l <108667457+DarkSquirr3l@users.noreply.github.com>
2023-10-26 14:22:06 +0100

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -2,142 +2,91 @@ import machine
 import utime
 import _thread
 
-# identify button location
-green_button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+# Button and LED pins
+button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+all_leds = [machine.Pin(pin, machine.Pin.OUT) for pin in [16, 17, 18, 19, 20, 21, 22, 26, 27, 28]]
+yel_leds = [machine.Pin(pin, machine.Pin.OUT) for pin in [16, 17, 18, 19, 20]]
+red_leds = [machine.Pin(pin, machine.Pin.OUT) for pin in [21, 22, 26, 27, 28]]
 
-# global button definition
-global button_pressed
+
+# Global button state
 button_pressed = False
 
-# identify led location
-yel_1_led = machine.Pin(16, machine.Pin.OUT)
-yel_2_led = machine.Pin(17, machine.Pin.OUT)
-yel_3_led = machine.Pin(18, machine.Pin.OUT)
-yel_4_led = machine.Pin(19, machine.Pin.OUT)
-yel_5_led = machine.Pin(20, machine.Pin.OUT)
-red_1_led = machine.Pin(21, machine.Pin.OUT)
-red_2_led = machine.Pin(22, machine.Pin.OUT)
-red_3_led = machine.Pin(26, machine.Pin.OUT)
-red_4_led = machine.Pin(27, machine.Pin.OUT)
-red_5_led = machine.Pin(28, machine.Pin.OUT)
-
+# Button reader thread
 def button_reader_thread():
     global button_pressed
     while True:
-        if green_button.value() == 1:
+        if button.value() == 1:
             button_pressed = True
         utime.sleep(0.01)
-        
+
 _thread.start_new_thread(button_reader_thread, ())
 
+def led_on(leds):
+    for led in leds:
+        led.value(1)
+
+def led_off(leds):
+    for led in leds:
+        led.value(0)
+
+# All leds of to start
+led_off(all_leds)
+
+# Main loop
 i = 1
 while True:
-    
-    if button_pressed == True:
+    if button_pressed:
         i += 1
         if i > 3:
             i = 1
         button_pressed = False
-    
-    if i == 1:
-        yel_1_led.value(1)
-        yel_2_led.value(1)
-        yel_3_led.value(1)
-        yel_4_led.value(1)
-        yel_5_led.value(1)
-        utime.sleep(1)
-        yel_1_led.value(0)
-        yel_2_led.value(0)
-        yel_3_led.value(0)
-        yel_4_led.value(0)
-        yel_5_led.value(0)
-        red_1_led.value(1)
-        red_2_led.value(1)
-        red_3_led.value(1)
-        red_4_led.value(1)
-        red_5_led.value(1)
-        utime.sleep(1)
-        red_1_led.value(0)
-        red_2_led.value(0)
-        red_3_led.value(0)
-        red_4_led.value(0)
-        red_5_led.value(0)
-    elif i == 2:
+
+    if i < 3:
         for j in range (10):
-            yel_1_led.value(1)
-            yel_2_led.value(1)
-            yel_3_led.value(1)
-            yel_4_led.value(1)
-            yel_5_led.value(1)
-            utime.sleep(0.2)
-            yel_1_led.value(0)
-            yel_2_led.value(0)
-            yel_3_led.value(0)
-            yel_4_led.value(0)
-            yel_5_led.value(0)
-            red_1_led.value(1)
-            red_2_led.value(1)
-            red_3_led.value(1)
-            red_4_led.value(1)
-            red_5_led.value(1)
-            utime.sleep(0.2)
-            red_1_led.value(0)
-            red_2_led.value(0)
-            red_3_led.value(0)
-            red_4_led.value(0)
-            red_5_led.value(0)
+            led_on(red_leds)
+            led_off(yel_leds)
+            utime.sleep(0.2 if i == 2 else 0.5)
+
+            led_off(red_leds)
+            led_on(yel_leds)
+            utime.sleep(0.2 if i == 2 else 0.5)
+        
+    if i == 2:
         for k in range (20):
-            yel_1_led.value(1)
-            yel_2_led.value(1)
-            yel_3_led.value(1)
-            yel_4_led.value(1)
-            yel_5_led.value(1)
+            led_on(red_leds)
+            led_off(yel_leds)
             utime.sleep(0.05)
-            yel_1_led.value(0)
-            yel_2_led.value(0)
-            yel_3_led.value(0)
-            yel_4_led.value(0)
-            yel_5_led.value(0)
-            red_1_led.value(1)
-            red_2_led.value(1)
-            red_3_led.value(1)
-            red_4_led.value(1)
-            red_5_led.value(1)
+
+            led_off(red_leds)
+            led_on(yel_leds)
             utime.sleep(0.05)
-            red_1_led.value(0)
-            red_2_led.value(0)
-            red_3_led.value(0)
-            red_4_led.value(0)
-            red_5_led.value(0)
-    else:
-        yel_1_led.value(1)
-        yel_2_led.value(1)
-        yel_3_led.value(1)
-        yel_4_led.value(1)
-        yel_5_led.value(1)
-        red_1_led.value(1)
-        red_2_led.value(1)
-        red_3_led.value(1)
-        red_4_led.value(1)
-        red_5_led.value(1)
-        utime.sleep(0.2)
-        yel_1_led.value(0)
-        yel_2_led.value(0)
-        utime.sleep(0.2)
-        red_1_led.value(0)
-        red_2_led.value(0)
-        yel_1_led.value(1)
-        yel_2_led.value(1)
-        utime.sleep(0.2)
-        red_1_led.value(1)
-        red_2_led.value(1)
-        red_3_led.value(0)
-        red_4_led.value(0)
-        red_5_led.value(0)
-        utime.sleep(0.2)
-        yel_1_led.value(1)
-        yel_2_led.value(1)
-        yel_3_led.value(0)
-        yel_4_led.value(0)
-        yel_5_led.value(0)
-        utime.sleep(0.2)
+
+    if i == 3: 
+        for led in all_leds:
+            led.value(1)
+            utime.sleep(0.1)
+            led.value(0)
+            
+        l = 0
+        m = 5
+        count = 0
+        while count < 5:
+            print(f"L {l}")
+            print(f"M {m}")
+            print(f"Count {count}")
+            all_leds[l].value(1)
+            utime.sleep(0.1)
+            all_leds[l].value(0)
+            l += 1
+            all_leds[m].value(1)
+            utime.sleep(0.1)
+            all_leds[m].value(0)
+            m += 1
+            count += 1
+        
+        l = 0
+        m = 5
+        count = 0
+                
+            
